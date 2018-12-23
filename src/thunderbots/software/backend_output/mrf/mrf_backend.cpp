d2ff0e99c8302f23091cc079402ebb5abbcf5fef
Henry Liu <henryymliu@gmail.com>
2018-12-22 17:41:53 -0800

    One more comment

--- src/thunderbots/software/backend_output/mrf/mrf_backend.cpp
+++ src/thunderbots/software/backend_output/mrf/mrf_backend.cpp
@@ -28,6 +28,9 @@ void MrfBackend::update_ball(Ball b)
 
 void MrfBackend::send_vision_packet()
 {
+
+/* TODO: Change handling of timestamp depending on age of team vs ball */
+/* Also see issue #150 */
     dongle.send_camera_packet(detbots, ball.position(), 
         std::chrono::duration_cast<std::chrono::milliseconds>(
                 ball.lastUpdateTimestamp().time_since_epoch()).count());
