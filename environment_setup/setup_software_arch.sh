0d9e18565aca5c977262a74e1097d33217a8dc25
Henry Liu <henryymliu@gmail.com>
2019-10-05 15:45:59 -0700

    first test

---
+++ environment_setup/setup_software_arch.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# UBC Thunderbots Linux Software Setup
+#
+# This script must be run with sudo! root permissions are required to install
+# packages and copy files to the /etc/udev/rules.d directory. The reason that the script
+# must be run with sudo rather than the individual commands using sudo, is that
+# when running CI within Docker, the sudo command does not exist since
+# everything is automatically run as root.
+#
+# This script will install all the required libraries and dependencies to build
+# and run the Thunderbots codebase. This includes being able to run the ai and
+# unit tests
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+
+# Save the parent dir of this so we can always run commands relative to the
+# location of this script, no matter where it is called from. This
+# helps prevent bugs and odd behaviour if this script is run through a symlink
+# or from a different directory.
+CURR_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+cd $CURR_DIR
+# The root directory of the reopsitory and git tree
+GIT_ROOT="$CURR_DIR/.."
+
+
+echo "================================================================"
+echo "Installing Utilities and Dependencies"
+echo "================================================================"
+
+host_software_packages=(
+    curl
+    cmake
+    protobuf
+    libusb
+    qt5-base # The GUI library for our visualizer
+    eigen # A math / numerical library used for things like linear regression
+    bazel
+    python-yaml # yaml for cfg generation (Dynamic Parameters)
+)
+sudo pacman -S ${host_software_packages[@]}
+
+if [ $? -ne 0 ]; then
+    echo "##############################################################"
+    echo "Error: Installing utilities and dependencies failed"
+    echo "##############################################################"
+    exit 1
+fi
+
+# Done
+echo "================================================================"
+echo "Done Software Setup"
+echo "================================================================"
+
